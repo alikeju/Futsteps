4ae19588ee4e0e6bfc8b57ce16b02fcf7793202e
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-08 13:53:16 -0400

    Updating Files

--- Futsteps/CreateMemberViewController.swift
+++ Futsteps/CreateMemberViewController.swift
@@ -24,7 +24,6 @@ class CreateMemberViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.loggedInUser = Auth.auth().currentUser
         configureView()
     }
     
@@ -35,7 +34,6 @@ class CreateMemberViewController: UIViewController {
         override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
             if (segue.identifier == "findOrgsSegue"){
                 let showFindOrgsTableViewController = segue.destination as! FindOrgsTableViewController
-                
                 showFindOrgsTableViewController.loggedInUser = self.loggedInUser as? FIRUser
             }
         }
@@ -57,22 +55,25 @@ class CreateMemberViewController: UIViewController {
         
         print(self)
         //
-        AuthService.createUser(controller: self, email: email, password: password) { (authOrg) in
-            guard let firOrg = authOrg else {
+        AuthService.createUser(controller: self, email: email, password: password) { (authMember) in
+            guard let firMember = authMember else {
                 return
             }
             
-            MemberService.create(firOrg, email: email, username: username, password: password) { (member) in
+            MemberService.create(firMember: firMember, email: email, username: username, password: password) { (member) in
                 guard let member = member else {
                     return
                 }
                 
                 Member.setCurrent(member, writeToUserDefaults: true)
+                self.loggedInUser = Auth.auth().currentUser
                 
-                let initialViewController = UIStoryboard.initialViewController(for: .main)
-                print("Member was created.")
-                self.view.window?.rootViewController = initialViewController
-                self.view.window?.makeKeyAndVisible()
+                
+                self.performSegue(withIdentifier: "findOrgsSegue", sender: self)
+//                let initialViewController = UIStoryboard.initialViewController(for: .main)
+//                print("Member was created.")
+//                self.view.window?.rootViewController = initialViewController
+//                self.view.window?.makeKeyAndVisible()
             
             }
         }
