befd66791bbfb70c42cc7500af275d587c3a73cc
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-10 10:43:51 -0400

    Updating Files

---
+++ Futsteps/Street.swift
@@ -0,0 +1,18 @@
+//
+//  Street.swift
+//  Futsteps
+//
+//  Created by Alikeju Adejo on 8/10/17.
+//  Copyright © 2017 Alikeju Adejo. All rights reserved.
+//
+
+import Foundation
+class Street {
+    var streetName = ""
+    var memberName = ""
+    var numberOfDoors = ""
+    var timeElapsed = ""
+    var sideOfStreet = ""
+    var content = ""
+    var modificationTime = Date()
+}
