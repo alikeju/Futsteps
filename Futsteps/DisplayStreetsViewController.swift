d0bcc51e807b56f9bbb60fb8791f9a8c2e34e8a9
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-10 16:52:04 -0400

    Updating Files

--- Futsteps/DisplayStreetsViewController.swift
+++ Futsteps/DisplayStreetsViewController.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 import UIKit
+import FirebaseDatabase
+import Firebase
+
+//post Struct was here moved to different location.
 
 class DisplayStreetsViewController: UIViewController{
     
+    @IBOutlet weak var streetNameTextField: UITextField!
     @IBOutlet weak var memberNameTextField: UITextField!
     @IBOutlet weak var numberOfDoorsTextField: UITextField!
     @IBOutlet weak var timeElapsedTextField: UITextField!
     @IBOutlet weak var sideOfStreetSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var commentsTextView: UITextView!
     
     var side = ""
     
@@ -23,33 +29,33 @@ class DisplayStreetsViewController: UIViewController{
         configureView()
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let identifier = segue.identifier {
-            if identifier == "cancel" {
-                print("Cancel button tapped")
-            } else if identifier == "save" {
-                print("Save button tapped")
-                
-                // 1
-                let street = Street()
-                
-                street.memberName = memberNameTextField.text ?? ""
-                street.numberOfDoors = numberOfDoorsTextField.text ?? ""
-                street.timeElapsed = timeElapsedTextField.text ?? ""
-                switch sideOfStreetSegmentedControl.selectedSegmentIndex{
-                case 0:
-                    side = "Odd"
-                case 1:
-                    side = "Even"
-                case 2:
-                    side = "Both"
-                default:
-                    break
-                }
-                street.modificationTime = Date()
-            }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    @IBAction func enterButtonTapped(_ sender: Any) {
+        let streetName = streetNameTextField.text
+        let name = memberNameTextField.text
+        let numOfDoors = numberOfDoorsTextField.text
+        let timeElapsed = timeElapsedTextField.text
+        let comments = commentsTextView.text
+        switch sideOfStreetSegmentedControl.selectedSegmentIndex{
+        case 0:
+            side = "Odd"
+        case 1:
+            side = "Even"
+        case 2:
+            side = "Both"
+        default:
+            break
+        }
+        PostService.create(streetName: streetName!, name: name!, numOfDoors: numOfDoors!, timeElapsed: timeElapsed!, sideOfStreet: side, comments: comments!) { (key) in
+            let firUser = Auth.auth().currentUser
+            PostListService.create(firUser: firUser!, postRef: key)
         }
     }
+
 }
 
 extension DisplayStreetsViewController{
@@ -57,4 +63,5 @@ extension DisplayStreetsViewController{
         applyKeyboardDismisser()
     }
 }
+
 
