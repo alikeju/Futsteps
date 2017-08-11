086cce11e48c5c20054d9532353f1f9f86e42f11
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-11 18:35:48 -0400

    Updating Files

--- Futsteps/AddService.swift
+++ Futsteps/AddService.swift
@@ -11,19 +11,8 @@ import FirebaseDatabase
 
 struct AddService {
     private static func addOrg(_ org: Organization, forCurrentUserWithSuccess success: @escaping (Bool) -> Void) {
-//        let addAttrs = ["organization_name": org]
-//        let ref = Database.database().reference().child("member_profiles").child(org.uid)
         let currentUID = Member.current.uid
-        let addData = ["member_profiles/\(currentUID)/\(org.uid)" : true]
-     //WHAT IF: let addData = ["member_profiles/\(currentUID)/\": ]
-//        ref.updateChildValues(addAttrs) { (error, _) in
-//            if let error = error{
-//                assertionFailure(error.localizedDescription)
-//            }
-//                success(error == nil)
-//        }
-//        
-//    }
+        let addData = ["member_profiles/\(currentUID)/organization_name" : org.organization]
         
         let ref = Database.database().reference()
         ref.updateChildValues(addData) { (error, _) in
