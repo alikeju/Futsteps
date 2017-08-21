871a4b5e7772fcd2f1c214870a77db45e4900284
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-21 10:55:40 -0400

    Updating Files

--- Futsteps/CreateMemberViewController.swift
+++ Futsteps/CreateMemberViewController.swift
@@ -1,18 +1,18 @@
  //
-//  OrgProfileViewController.swift
-//  Futsteps
-//
-//  Created by Alikeju Adejo on 7/31/17.
-//  Copyright © 2017 Alikeju Adejo. All rights reserved.
-//
-
-import UIKit
-import Firebase
-
-
-class CreateMemberViewController: UIViewController {
+ //  OrgProfileViewController.swift
+ //  Futsteps
+ //
+ //  Created by Alikeju Adejo on 7/31/17.
+ //  Copyright © 2017 Alikeju Adejo. All rights reserved.
+ //
+ 
+ import UIKit
+ import Firebase
+ 
+ class CreateMemberViewController: UIViewController {
     
-    //var activityIndicator: uI
+    
+    var indicator = UIActivityIndicatorView()
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
@@ -21,7 +21,7 @@ class CreateMemberViewController: UIViewController {
     
     var loggedInUser:AnyObject?
     var databaseRef = Database.database().reference()
-  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,16 +32,23 @@ class CreateMemberViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            if (segue.identifier == "findOrgsSegue"){
-                let showFindOrgsTableViewController = segue.destination as! FindOrgsTableViewController
-                showFindOrgsTableViewController.loggedInUser = self.loggedInUser as? FIRUser
-            }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "findOrgsSegue"){
+            let showFindOrgsTableViewController = segue.destination as! FindOrgsTableViewController
+            showFindOrgsTableViewController.loggedInUser = self.loggedInUser as? FIRUser
         }
+    }
+    
+    func showActivityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0))
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+    }
     
     
     @IBAction func enterButton(_ sender: Any) {
-    
+        
         guard let email = emailTextField.text,
             let username = usernameTextField.text,
             let password = passwordTextField.text,
@@ -53,6 +60,9 @@ class CreateMemberViewController: UIViewController {
                 print("Please fill all fields!")
                 return
         }
+        self.showActivityIndicator()
+        self.indicator.startAnimating()
+        self.indicator.backgroundColor = UIColor.white
         
         print(self)
         //
@@ -60,7 +70,7 @@ class CreateMemberViewController: UIViewController {
             guard let firMember = authMember else {
                 return
             }
-        
+            
             MemberService.create(firMember: firMember, email: email, username: username, password: password) { (member) in
                 guard let member = member else {
                     return
@@ -71,14 +81,17 @@ class CreateMemberViewController: UIViewController {
                 self.loggedInUser = Auth.auth().currentUser
                 
                 self.performSegue(withIdentifier: "findOrgsSegue", sender: self)
-            
-            }  
+                
+                self.indicator.stopAnimating()
+                self.indicator.hidesWhenStopped = true
+            }
         }
     }
-}
-
-extension CreateMemberViewController{
+    
+ }
+ 
+ extension CreateMemberViewController{
     func configureView(){
         applyKeyboardDismisser()
     }
-}
+ }
