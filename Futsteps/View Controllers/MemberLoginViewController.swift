3a8a76233e5f372ed66ad6eb72808edceb643ccc
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-08-28 10:18:35 -0400

    Updating Files

--- Futsteps/View Controllers/MemberLoginViewController.swift
+++ Futsteps/View Controllers/MemberLoginViewController.swift
@@ -14,12 +14,22 @@ class MemberLoginViewController: UIViewController{
     @IBOutlet weak var emailTextField: UITextField!
     
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    var indicator = UIActivityIndicatorView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureView()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func showActivityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRect(x: 0.0, y: 0.0, width: 80.0, height: 80.0))
+        indicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        indicator.center = self.view.center
+        self.view.addSubview(indicator)
+    }
+    
 
     @IBAction func enterButton(_ sender: Any) {
         guard let email = emailTextField.text,
@@ -27,6 +37,11 @@ class MemberLoginViewController: UIViewController{
             else{
             return
         }
+        
+        enterButton.isUserInteractionEnabled = false
+        self.showActivityIndicator()
+        self.indicator.startAnimating()
+        self.indicator.backgroundColor = UIColor.white
         
         AuthService.signIn(controller: self, email: email, password: password) { (member) in
             guard let member = member else {
