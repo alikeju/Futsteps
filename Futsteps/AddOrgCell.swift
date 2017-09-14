f53ea1a7a7aea6f491199c5e21fc5867cfab9302
Alikeju Adejo <alikejuadejo@yahoo.com>
2017-09-14 11:52:42 -0400

    Updating Files

--- Futsteps/AddOrgCell.swift
+++ Futsteps/AddOrgCell.swift
@@ -11,31 +11,17 @@ import Foundation
 import UIKit
 
 protocol AddOrgCellDelegate: class {
-    func didTapAddButton(_ addButton: UIButton, on cell: AddOrgCell)
+    func showAlert(title:String, message:String);
 }
 
 class AddOrgCell: UITableViewCell {
-    
-    weak var delegate: AddOrgCellDelegate?
-    
-    @IBOutlet weak var addButton: UIButton!
-    @IBOutlet weak var orgNameLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        addButton.layer.borderColor = UIColor.lightGray.cgColor
-        addButton.layer.borderWidth = 1
-        addButton.layer.cornerRadius = 6
-        addButton.clipsToBounds = true
-        
-        addButton.setTitle("Added", for: .normal)
-        addButton.setTitle("Added", for: .selected)
-    }
-    
-    @IBAction func addButtonTapped(_ sender: Any) {
-        delegate?.didTapAddButton(sender as! UIButton, on: self)
-    }
+    var delegate: AddOrgCellDelegate?
 
-
+    //delegate.showAlert()
 }
+
+//class SomeCell: UITableViewCell {
+//    var delegate: AudioPlayable?
+//    @IBAction func userDidSomething(sender: AnyObject) {
+//        delegate?.playSoundsIfAble()
+//}
